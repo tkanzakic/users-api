a8b3109514528f75c1fde11dc7023dfe77498240
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-04-16 16:10:22 +0200

    filter users by emailContains

--- src/handlers/users/users.js
+++ src/handlers/users/users.js
@@ -3,7 +3,17 @@ import { fetch } from '../../model/users';
 export const get = async (ctx, next) => {
   ctx.status = 200;
   const users = await fetch();
-  ctx.body = { users };
+  const filtered = applyFilters(users, ctx.query);
+  ctx.body = { users: filtered };
   await next();
   return ctx.body;
+};
+
+const applyFilters = (users, params) => {
+  if (params === undefined) return users;
+  var filtered = users;
+  if (params.emailContains) {
+    filtered = filtered.filter((user) => user.email.includes(params.emailContains));
+  }
+  return filtered;
 };
--- src/handlers/users/users.test.js
+++ src/handlers/users/users.test.js
@@ -1,17 +1,73 @@
 import { get } from '.';
 
-describe('users.get', () => {
-  const mockCtx = {
-    request: {
-      header: {
-        'x-bundle-access-token': 'token',
+const mockUsers = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@gmail.com',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
       },
     },
-  };
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    address: {
+      street: 'Victor Plains',
+      suite: 'Suite 879',
+      city: 'Wisokyburgh',
+      zipcode: '90566-7771',
+      geo: {
+        lat: '-43.9509',
+        lng: '-34.4618',
+      },
+    },
+    phone: '010-692-6593 x09125',
+    website: 'anastasia.net',
+    company: {
+      name: 'Deckow-Crist',
+      catchPhrase: 'Proactive didactic contingency',
+      bs: 'synergize scalable supply-chains',
+    },
+  },
+];
+jest.mock('../../model/users', () => ({
+  fetch: async () => Promise.resolve(mockUsers),
+}));
+
+describe('users.get', () => {
   const noop = () => {};
-  test('should set status to 200', async () => {
+  test('should set status to 200 and return users', async () => {
+    const mockCtx = {};
     await get(mockCtx, noop);
     expect(mockCtx.status).toBe(200);
-    expect(mockCtx.body.users).toBeDefined();
+    expect(mockCtx.body.users).toStrictEqual(mockUsers);
+  });
+  test('should filter users by email if emailContains filter provided', async () => {
+    const mockCtx = {
+      query: {
+        emailContains: 'gmail',
+      },
+    };
+    await get(mockCtx, noop);
+    expect(mockCtx.status).toBe(200);
+    expect(mockCtx.body.users).toStrictEqual([mockUsers[0]]);
   });
 });
